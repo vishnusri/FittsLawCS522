ac8e98fc92b3f35d8450a2979b0f6f025320573a
vishnusri <vishnusri@gmail.com>
2017-03-27 16:00:24 -0500

    Add files via upload
    Randomised squares
    change target size

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ $(".form").submit(function(e) {
                   });
 
 $('#start').click(function(){
-    updatePattern();
-    select_random(); 
+    var a = $("#size_1")["0"].value;
+    var b = $("#size_2")["0"].value;
+    var c = $("#click_num")["0"].value;
+    if((a%1==0)&&(parseInt(a)<=$("#size_1")["0"].max)&&(parseInt(a)>=$("#size_1")["0"].min)){
+       if((b%1==0)&&(parseInt(b)<=$("#size_2")["0"].max)&&(parseInt(b)>=$("#size_2")["0"].min)){
+           if((c%1==0)&&(parseInt(c)<=$("#click_num")["0"].max)&&(parseInt(c)>=$("#click_num")["0"].min)){
+               trial++;
+               datum.push({"trial": trial+1, "clicks":[]});
+                count = parseInt(c)*2;
+                mincount = parseInt(c);
+                updatePattern();
+                select_random();   
+           } } 
+    }
+   
     //name in index.html 'start'
 });
 
@@ -12,9 +25,13 @@ $('#start').click(function(){
 
 var width = 500,
     height = 500,
-    a = 100,
+    s,
     rows,
-    columns;
+    columns,
+    count=20,
+    mincount=10,
+    trial=0,
+    datum = [{"trial": trial+1, clicks:[]}];
 
 var svg = d3.select("body")
             .append("svg")
@@ -22,31 +39,37 @@ var svg = d3.select("body")
             .attr("height",height);
 
 
-//var squarepath = "M " + 0 + " " + 0 + " L " + a + " " + 0 + " L " + a + " " + a + " L " + 0  + " " + a + " Z" ;
+//var squarepath = "M " + 0 + " " + 0 + " L " + s + " " + 0 + " L " + s + " " + s + " L " + 0  + " " + s + " Z" ;
 
 var squarepaths;
 var previous, present="nothing";
-var squares = function(width, height, a){
+var squares = function(width, height, s){
     squarepaths=[];
-    rows = (Math.floor(width/a));
-    columns = (Math.floor(height/a));
+    rows = (Math.floor(width/s));
+    columns = (Math.floor(height/s));
     //console.log(columns);
     for(i = 0; i < rows; i++){
         for(j=0; j<columns; j++){
             temp= [i , j];
-            temp.push("M " + (a*i) + " " + (a*j) + " L " + (a+(a*i)) + " " + (a*j) + " L " + (a+(a*i)) + " " + (a+(a*j)) + " L " + (a*i)  + " " + (a+(a*j)) + " Z");
+            temp.push("M " + (s*i) + " " + (s*j) + " L " + (s+(s*i)) + " " + (s*j) + " L " + (s+(s*i)) + " " + (s+(s*j)) + " L " + (s*i)  + " " + (s+(s*j)) + " Z");
             squarepaths.push(temp);
         }
     }          
 }
 
-
+squares(width, height,s);
 updatePattern();
 select_random();
 
 function updatePattern(){
     
+    if(count>mincount){
     squares(width,height,parseInt($("#size_1")["0"].value));
+    }
+    else if(count==mincount){
+    squares(width,height,parseInt($("#size_2")["0"].value));    
+    }
+   
     
     d3.selectAll("path").remove();
 
@@ -64,30 +87,70 @@ function updatePattern(){
     //select_random();
     
     //console.log(present);
+    
+    $(".square").click(function(){
+    if(this.classList.contains("present")){
+        var t = new Date;
+        
+        if (count>=mincount){
+            datum[trial].clicks[(mincount*2)-count-1] = {};
+            datum[trial].clicks[(mincount*2)-count-1]["size"] = parseInt($("#size_1")["0"].value);
+        }
+        
+        else if (count>0){
+            datum[trial].clicks[(mincount*2)-count-1] = {};
+            datum[trial].clicks[(mincount*2)-count-1]["size"] = parseInt($("#size_2")["0"].value);
+        }
+        
+        datum[trial].clicks[(mincount*2)-count-1]["absTime"] = t.getTime();
+        datum[trial].clicks[(mincount*2)-count-1]["previous"] = previous;
+        datum[trial].clicks[(mincount*2)-count-1]["present"] = present;
+        
+        console.log(datum);
+        
+        if(count==mincount){
+            updatePattern();
+            select_random(); 
+        
+        }
+        else if(count>=0){
+            select_random(); 
+        
+        }
+}
+});
         
 
 }
 
-$(".square").click(function(){
-    if(this.classList.contains("present")){
-     select_random();   
-}
-});
+<!-- ----------------- -->
 
 function select_random(){
+    console.log(count);
+    if(count!=0){
     if((previous!="nothing") && (previous!="undefined")){
       $("#"+previous).removeClass("previous");
     }
     
     previous=present;
-    present = Math.floor(Math.random()*rows)+ "_" + Math.floor(Math.random()*columns);
-    
+    temp = Math.floor(Math.random()*rows)+ "_" + Math.floor(Math.random()*columns);
+    if(temp==previous){
+        var first = parseInt(temp.split('_')[0]);
+        var second = parseInt(temp.split('_')[1]);
+        second = (second == columns - 1) ? (second - 1) : (second + 1);
+        console.log("true");
+        temp = first + "_" + second;
+    }
+    present = temp;
     if((previous!="nothing") && (previous!="undefined")){
       $("#"+previous).removeClass("present"); 
       $("#"+previous).addClass("previous");         
     }
     console.log("previous:" + previous + ",present: " + present);
+    count--;
     $("#"+present).addClass("present");
-    
-
+    }
+    else{
+        $("#" + present).removeClass("present");
+    }
 }
